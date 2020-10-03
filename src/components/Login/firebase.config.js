92f5e76522f1af79a53407c3264b866e7e2f7fea
SShohag <shohag7m@gmail.com>
2020-10-03 09:59:59 +0600

    first voluteer commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvEW4VxTAwHkAcJow9dp8OZSjgmSJ8huU",
+    authDomain: "volunteer-webbing.firebaseapp.com",
+    databaseURL: "https://volunteer-webbing.firebaseio.com",
+    projectId: "volunteer-webbing",
+    storageBucket: "volunteer-webbing.appspot.com",
+    messagingSenderId: "230569300970",
+    appId: "1:230569300970:web:3bbd3195fbce301281d893"
+  };
+
+  export default firebaseConfig;
---
+++ src/fakeData/volunteerInfomation.js
@@ -0,0 +1,124 @@
+var volunteers = [
+    {
+        "key":"5adKcIQV",
+        "type":"Child Support",
+        "name":"Child Support",
+        "img":"https://i.ibb.co/vLyXdXv/child-Support.png"
+    },
+    {
+        "key":"ItP1oAbE",
+        "type":"Refuge Shelter",
+        "name":"Refuge Shelter",
+        "img":"https://i.ibb.co/zbRYHSt/refuse-Shelter.png"
+    },
+    {
+        "key":"eLkjBf2M",
+        "type":"Food Charity",
+        "name":"Food Charity",
+        "img":"https://i.ibb.co/HxZz9hz/food-Charity.png"
+    },
+    {
+        "key":"9C1zulso",
+        "type":"Host and Clothing Swap",
+        "name":"Host and Clothing Swap",
+        "img":"https://i.ibb.co/gzBjmJV/cloth-Swap.png"
+    },
+    {
+        "key":"eovOdIAZ",
+        "type":"Host a River Clean-up",
+        "name":"Host a River Clean-up",
+        "img":"https://i.ibb.co/m9Fq3XF/river-Clean.png"
+    },
+    {
+        "key":"nqtABou4",
+        "type":"Clean Water for Children",
+        "name":"Clean Water for Children",
+        "img":"https://i.ibb.co/vkG0yz7/clean-Water.png"
+    },
+    {
+        "key":"Vdwbaxmi",
+        "type":"Good Education",
+        "name":"Good Education",
+        "img":"https://i.ibb.co/0ybkMZG/good-Education.png"
+    },
+    {
+        "key":"gV9Zw4ke",
+        "type":"New Books for Children",
+        "name":"New Books for Children",
+        "img":"https://i.ibb.co/vw8gbny/newBooks.png"
+    },
+    {
+        "key":"NpCmEO3Y",
+        "type":"Host a Study Group",
+        "name":"Host a Study Group",
+        "img":"https://i.ibb.co/xmzrmRn/study-Group.png"
+    },
+    {
+        "key":"CXgqi4UW",
+        "type":"Build Birdhouses for Your Neighbor",
+        "name":"Build Birdhouses for Your Neighbor",
+        "img":"https://i.ibb.co/kJ7d4Rg/bird-House.png"
+    },
+    {
+        "key":"ZsC3lzOi",
+        "type":"Organize Books at the Library",
+        "name":"Organize Books at the Library",
+        "img":"https://i.ibb.co/prRXksc/library-Books.png"
+    },
+    {
+        "key":"bFZt3MS1",
+        "type":"Give a seminar for Driving Safety",
+        "name":"Give a seminar for Driving Safety",
+        "img":"https://i.ibb.co/yySnhYs/drive-Safety.png"
+    },
+    {
+        "key":"zNaMFJpu",
+        "type":"Give free Music Lessons",
+        "name":"Give free Music Lessons",
+        "img":"https://i.ibb.co/FVR1KYp/music-Lessons.png"
+    },
+    {
+        "key":"EAZySiv9",
+        "type":"Take People How to Register to vote",
+        "name":"Take People How to Register to vote",
+        "img":"https://i.ibb.co/SRsS4wG/vote-Register.png"
+    },
+    {
+        "key":"0tYoJLw9",
+        "type":"Clean-up Your Local Park",
+        "name":"Clean-up Your Local Park",
+        "img":"https://i.ibb.co/Jdfg1vx/clearn-Park.png"
+    },
+    {
+        "key":"PCaHk9sg",
+        "type":"Give it Help to Local Adults",
+        "name":"Give it Help to Local Adults",
+        "img":"https://i.ibb.co/jvbHkBS/ITHelp.png"
+    },
+    {
+        "key":"oxv8pfVG",
+        "type":"Foster a Shelter Animals",
+        "name":"Foster a Shelter Animals",
+        "img":"https://i.ibb.co/Nj9mjzc/animal-Shelter.png"
+    },
+    {
+        "key":"ACbXr5on",
+        "type":"Babysit During PTA Meetings",
+        "name":"Babysit During PTA Meetings",
+        "img":"https://i.ibb.co/b20G78Q/babySit.png"
+    },
+    {
+        "key":"fWcyvAXa",
+        "type":"Collect stuffed Animals",
+        "name":"Collect stuffed Animals",
+        "img":"https://i.ibb.co/h89xD8c/stuffed-Animals.png"
+    },
+    {
+        "key":"Nrxjd2Gc",
+        "type":"Collect School Supplies",
+        "name":"Collect School Supplies",
+        "img":"https://i.ibb.co/R25Jr9N/school-Suffiles.png"
+    }
+    
+];
+export default volunteers;
